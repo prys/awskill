d0a95bf769f4362e944e9b2874d2891f7e4f2323
Prys Williams <pryswilliams@gmail.com>
2018-04-23 10:36:51 +0100

    started adding NAT G/w

--- awskill.py
+++ awskill.py
@@ -7,11 +7,16 @@ for regionDetails in response['Regions']:
     print('Looking in region ' + regionDetails['RegionName'])
     ec2 = boto3.client('ec2', regionDetails['RegionName'])
     ec2Response = ec2.describe_instances()
-    for i in range (len(ec2Response['Reservations'])):
-        thisInstance = ec2Response['Reservations'][i]['Instances'][0]['InstanceId']
-        thisState = ec2Response['Reservations'][i]['Instances'][0]['State']['Name']
+    for ins in ec2Response['Reservations']:
+        thisInstance = ins['Instances'][0]['InstanceId']
+        thisState = ins['Instances'][0]['State']['Name']
         termProt = ec2.describe_instance_attribute(Attribute='disableApiTermination', InstanceId=thisInstance)
         print('Found instance ' + thisInstance)
         if termProt and (thisState != 'terminated'):
             modResponse = ec2.modify_instance_attribute(Attribute='disableApiTermination', Value='False', InstanceId=thisInstance)
             termResponse = ec2.terminate_instances(InstanceIds=[thisInstance])
+    natResponse = ec2.describe_nat_gateways()
+    if len(natResponse['NatGateways']) != 0:
+        for natGWs in natResponse['NatGateways']:
+            print(natGWs['NatGatewayId'])
+            print(natGWs['State'])
